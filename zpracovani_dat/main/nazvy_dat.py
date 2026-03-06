d3e485cc772297a3c44f4a30d9c83bdee8b63f3d
Matozab <vmatousekv@gmail.com>
2026-03-06 21:32:07 +0100

    moje zmeny

---
+++ zpracovani_dat/main/nazvy_dat.py
@@ -0,0 +1,11 @@
+nazvy_sad = {
+    "okresy_ceny.csv": ["Index ceny","Cena za m2"],
+    "okresy_kvalita_ovzdusi.csv": "Kvalita ovzduší",
+    "okresy_kvalita_vody.csv": "Kvalita vody",
+    "okresy_kvalita_povrchovych_vod.csv": "Kvalita povrchových vod",
+    "okresy_kvalita_podzemnich_vod.csv": "Kvalita podzemních vod",
+    "okresy_kvalita_zivotniho_prostredi.csv": "Kvalita životního prostředí",
+}
+
+
+indexy_pro_vypocet = ["Index ceny","Kvalita ovzduší"]
