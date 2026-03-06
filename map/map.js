20135dd56fc5ed76f51fad3b7b137df798159091
AndrewKafka <ondrakafka@gmail.com>
2026-03-06 14:55:54 +0100

    header, footer + logo

---
+++ map/map.js
@@ -0,0 +1,8 @@
+var map = L.map('main_map').setView([50.08804, 14.42076], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+}).addTo(map);
+
+// L.marker([50.08804, 14.42076]).addTo(map)
+//     .bindPopup("Hello from Prague!")
+//     .openPopup();
