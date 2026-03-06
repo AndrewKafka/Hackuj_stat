4d9e272792307e1900182c07e5dae8ee8cb831da
AndrewKafka <ondrakafka@gmail.com>
2026-03-06 17:08:18 +0100

    working button to switch side panel

--- map/map.js
+++ map/map.js
@@ -7,5 +7,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     .bindPopup("Hello from Prague!")
 //     .openPopup();
 
+const sidePanel = document.getElementById('side_panel');
+const toggleBtn = document.getElementById('toggle_panel');
+const mapDiv = document.getElementById('main_map');
 
-document.getElementById("main_info").style.height = document.getElementById("main_map").clientHeight + "px";
+toggleBtn.addEventListener('click', () => {
+    sidePanel.classList.toggle('collapsed');
+    
+    if(sidePanel.classList.contains('collapsed')){
+        // collapsed → leave 40px visible
+        mapDiv.style.marginLeft = '-240px';
+        console.log('collapsed');
+    } else {
+        mapDiv.style.marginLeft = '0px'; // full sidebar width
+    }
+
+    // refresh leaflet map
+    setTimeout(() => map.invalidateSize(), 310);
+});
